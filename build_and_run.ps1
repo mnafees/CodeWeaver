92daca29aa35360605d3fdf6262e7c0cc3bae3a6
Carlos <tesserato@hotmail.com>
2024-09-04 13:10:01 +0100

    updated executable name to codeweaver

--- build_and_run.ps1
+++ build_and_run.ps1
@@ -1,6 +1,6 @@
 # mit the symbol table, debug information and the DWARF symbol table by passing -s and -w go build -ldflags="-s -w" .
 go build -ldflags="-s -w" .
 
-./codemerge -h
+./codeweaver -h
 
-./codemerge -ignore="\.git.*,.+\.exe" -excluded-paths-file="excluded_paths.txt"
+./codeweaver -ignore="\.git.*,.+\.exe" -excluded-paths-file="excluded_paths.txt"
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module codemerge
+module codeweaver
 
 go 1.23.0
