1b33b18aabf888a6bd3fc2494cb3637ce3945eb0
Carlos Tarjano <tesserato@hotmail.com>
2025-02-14 08:04:20 +0000

    changed flags location at goreleaser.yaml

--- build_and_run.ps1
+++ build_and_run.ps1
@@ -1,8 +1,8 @@
-# mit the symbol table, debug information and the DWARF symbol table by passing -s and -w go build -ldflags="-s -w" .
-go build # -ldflags="-s -w" .
+$version = "dev" 
+$commit = git rev-parse --short HEAD
+$date = Get-Date -format "yyyy-MM-dd HH:mm:ss"
 
-./CodeWeaver -h
+go build -ldflags "-X 'main.version=$version' -X 'main.commit=$commit' -X 'main.date=$date'" .
 
+./CodeWeaver -version
 ./CodeWeaver -ignore="\.git.*,.+\.exe,codebase.md,excluded_paths.txt" -excluded-paths-file="excluded_paths.txt"
-
-# "\.git.*,.+\.exe,.+\.[Ii][Cc][Oo],.+\.[Pp][Nn][Gg],.+\.[Jj][Pp][Ee]?[Gg],.+\.[Mm][Pp][34],.+\.[Aa][Vv][Ii],.+\.[Mm][Oo][Vv],.+\.[Ww][Mm][Aa],.+\.[Pp][Dd][Ff],.+\.[Dd][Oo][Cc][Xx]?,.+\.[Xx][Ll][Ss][Xx]?,.+\.[Pp][Pp][Tt][Xx]?,.+\.[Zz][Ii][Pp],.+\.[Rr][Aa][Rr],.+\.[7][Zz],.+\.[Ii][Ss][Oo],.+\.[Bb][Ii][Nn],.+\.[Dd][Aa][Tt],.+\.[Dd][Mm][Gg],.+\.[Gg][Ii][Ff],.+\.[Tt][Ii][Ff][Ff]?,.+\.[Pp][Ss][Dd],.+\.[Mm][Kk][Vv],.+\.[Ff][Ll][Aa][Cc],.+\.[Oo][Gg][Gg],.+\.[Ww][Ee][Bb][Pp],.+\.[Aa][Vv][Ii][Ff],.+\.[Hh][Ee][Ii][Cc],.+\.[Jj][Xx][Ll]"
