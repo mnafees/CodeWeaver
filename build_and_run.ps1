a2790f79368fae70a9f738e45498917ae4c3a02e
Carlos Tarjano <tesserato@hotmail.com>
2025-02-14 08:16:48 +0000

    changed timestamp format

--- build_and_run.ps1
+++ build_and_run.ps1
@@ -1,10 +1,10 @@
 $version = "dev" 
 $commit = git rev-parse --short HEAD
-$date = Get-Date -format "yyyy-MM-dd HH:mm:ss"
+$date = Get-Date -format "yyyy-MM-ddTHH:mm:ss"
 
 go build -ldflags "-X 'main.version=$version' -X 'main.commit=$commit' -X 'main.date=$date'" .
 
-# goreleaser build
+# goreleaser build --clean
 
 ./CodeWeaver -version
 ./CodeWeaver -ignore="\.git.*,.+\.exe,codebase.md,excluded_paths.txt" -excluded-paths-file="excluded_paths.txt"
