7ed2e29a160d2eddf3f2734389014ef46acdba22
Carlos Tarjano <tesserato@hotmail.com>
2025-02-14 08:11:45 +0000

    testing builds beforehand running goreleaser build

--- build_and_run.ps1
+++ build_and_run.ps1
@@ -4,5 +4,7 @@ $date = Get-Date -format "yyyy-MM-dd HH:mm:ss"
 
 go build -ldflags "-X 'main.version=$version' -X 'main.commit=$commit' -X 'main.date=$date'" .
 
+# goreleaser build
+
 ./CodeWeaver -version
 ./CodeWeaver -ignore="\.git.*,.+\.exe,codebase.md,excluded_paths.txt" -excluded-paths-file="excluded_paths.txt"
